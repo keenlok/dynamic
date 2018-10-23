8824d432426dcff90517121d70a4771ceb80b3e9
Keen Lok <keenloklai@gmail.com>
2018-10-23 14:33:35 +0800

    git ignore

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+var expandIPv6Address = require('./ipv6');
+
 if (null) {
   console.log("true")
 } else {
@@ -5,11 +7,13 @@ if (null) {
   console.log(+(new Date));
 
   // let string = "hellow/\//";
-  let string = RegExp('.*');
-  console.log('Before', string);
-  // string.replace(/ah/g, '\uffff')
-  // console.warn('\uffff')
-  console.log('After', string);
+  let string = /^\d/;
+  let addr4 = "0.0.0.0:12345";
+  let addr6 = ":::12345";
+  let local = "localhost:12345";
 
-  console.log(Math.max(0, Infinity || 0));
+  console.log('Ipv4', string.exec(addr4));
+  console.log('Ipv6', string.exec(addr6));
+  console.log('local', string.exec(local));
+  console.log('ipv6 expand', expandIPv6Address("::"));
 }
